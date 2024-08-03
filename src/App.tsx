4506552abe7155413c29f419742189d0a1c6bf02
lucas <santanafx@hotmail.com>
2024-08-02 22:34:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/Button/Button";
+
+function App() {
+  return (
+    <div>
+      <Button width="200px">Click</Button>
+    </div>
+  );
+}
+
+export default App;
