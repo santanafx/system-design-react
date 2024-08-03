53891096aa4837952852f5e2189f407e005082bc
lucas <santanafx@hotmail.com>
2024-08-03 07:01:37 -0300

    feat: testing form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from "./components/Button/Button";
+import { useForm } from "react-hook-form";
+import { Button } from "./components/atoms/Button/Button";
+import { Input } from "./components/atoms/Input/Input";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  const handleRegisterTest = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <Button width="200px">Click</Button>
+      <div>
+        <form onSubmit={handleSubmit(handleRegisterTest)}>
+          <Input name="firstName" register={register} />
+          <Button width="200px" type="submit">
+            Test Form
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
